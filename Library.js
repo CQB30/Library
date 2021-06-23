c244ee782063789b0dfd3393dc818cbb89b0d3af
cqb30 <cqb30al@gmail.com>
2021-06-23 19:34:11 -0400

    Building blocks V1

---
+++ Library.js
@@ -0,0 +1,23 @@
+const { table } = require("console");
+
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+let libraryTableBody = document.querySelector("#tableBody");
+
+function displayLibrary() {
+    for (book in myLibrary) {
+        document.createElement('tr');
+        
+    }
+}
